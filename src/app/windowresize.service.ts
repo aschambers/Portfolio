13751a8f8a66c09cd61f1e105475fd93f4312728
aschambers <allisonschambers20@gmail.com>
2024-06-08 19:30:16 -0700

    Update project style and change image names to be all lowercase for consistency with deployment

--- src/app/windowresize.service.ts
+++ src/app/windowresize.service.ts
@@ -8,11 +8,11 @@ export class WindowResizeService {
   handleWindowResize(element: string) {
     const pageElement = document.getElementById(element);
     if (pageElement && element === 'project-one-img') {
-      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/HomePageMobile.png' : 'assets/images/Zygarden.png';
+      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/homepagemobile.png' : 'assets/images/zygarden.png';
     } else if (pageElement && element === 'project-two-img') {
-      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/SleepVisualizerMobile.png' : 'assets/images/SleepVisualizer.png';
+      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/sleepvisualizermobile.png' : 'assets/images/sleepvisualizer.png';
     } else if (pageElement && element === 'project-three-img') {
-      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/ReplayVisualizerMobile.png' : 'assets/images/ReplayVisualizer.png';
+      (<HTMLImageElement>pageElement).src = window.innerWidth < ScreenSizes.small ? 'assets/images/replayvisualizermobile.png' : 'assets/images/replayvisualizer.png';
     }
   }
 }
